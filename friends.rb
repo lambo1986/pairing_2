76e91131f91143dd74d9920a70a124747c4b806c
Nathan Lambertson <lambertones@gmail.com>
2023-09-20 11:50:43 -0500

    Change the values of the variable named arrays

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Avi", "Slim", "Cami", "Reid", "Brittnee"]
 p arrays[2]
 p arrays[4]
 p arrays.first
+
+arrays = ["Iceland", "Florida", "Nepal", "Korea", "Alaska"]
+p arrays.push("Italy", "Morocco")
+  
